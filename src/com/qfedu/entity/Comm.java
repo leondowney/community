9a53392a5ca5e946ec909b6f6efabf29f2acfed7
“leondowney” <leondowney912@outlook.com>
2023-02-09 16:01:33 +0800

    Initial commit

---
+++ src/com/qfedu/entity/Comm.java
@@ -0,0 +1,9 @@
+package com.qfedu.entity;
+
+/**
+ * @author Leon Downey
+ * @date 2023/2/9 15:45
+ * @describe： 社区系统
+ */
+public class Comm {
+}
---
+++ src/com/qfedu/entity/Fac.java
@@ -0,0 +1,9 @@
+package com.qfedu.entity;
+
+/**
+ * @author Leon Downey
+ * @date 2023/2/9 15:46
+ * @describe：
+ */
+public class Fac {
+}
